b35c359fa1dfdb28814cb9b10e6c39c98d102cb3
Fahim-a1 <mm4516@srmist.edu.in>
2026-03-01 07:43:20 +0530

    Updated week 2 code

--- week2/uc6.java
+++ week2/uc6.java
@@ -1,3 +1,60 @@
+package week2;
 public class uc6 {
-    
+
+  public static void main(String[] args) {
+
+    String[] oPattern = getOPattern();
+    String[] pPattern = getPPattern();
+    String[] sPattern = getSPattern();
+
+    String[] banner = new String[7];
+
+    for (int i = 0; i < 7; i++) {
+      banner[i] = String.join("   ",
+          oPattern[i],
+          oPattern[i],
+          pPattern[i],
+          sPattern[i]);
+    }
+
+    for (String line : banner) {
+      System.out.println(line);
+    }
+  }
+
+  public static String[] getOPattern() {
+    return new String[] {
+        " ***** ",
+        "*     *",
+        "*     *",
+        "*     *",
+        "*     *",
+        "*     *",
+        " ***** "
+    };
+  }
+
+  public static String[] getPPattern() {
+    return new String[] {
+        " ******",
+        "*     *",
+        "*     *",
+        " ******",
+        "*      ",
+        "*      ",
+        "*      "
+    };
+  }
+
+  public static String[] getSPattern() {
+    return new String[] {
+        " ***** ",
+        "*     *",
+        "*      ",
+        " ***** ",
+        "      *",
+        "*     *",
+        " ***** "
+    };
+  }
 }
